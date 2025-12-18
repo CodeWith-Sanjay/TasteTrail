0341669ed9c8fa66f5d47445c5d3bb2b6138ba18
CodeWith-Sanjay <sanjayganesan016@gmail.com>
2025-12-18 23:19:18 +0530

    updated top and nav bar, working on hero section

--- tastetrail/src/Components/Navbar.jsx
+++ tastetrail/src/Components/Navbar.jsx
@@ -1,13 +1,17 @@
 import React, {useState} from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import '../styles/navbar.css';
 
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import HighlightOffRoundedIcon from '@mui/icons-material/HighlightOffRounded';
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
+
 
 const Navbar = () => {
 
+  const location = useLocation();
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
@@ -17,6 +21,7 @@ const Navbar = () => {
         <p>Contact us - <a href='mailto:sanjayganesan016@gmail.com'>sanjayganesan016@gmail.com</a> </p>
       </div>
 
+      <div className='nav-wrapper'>
       <div className='top-nav-header'>
 
         <div>
@@ -29,23 +34,30 @@ const Navbar = () => {
           <SearchRoundedIcon className='nav-search-icon'/>
         </div>
 
-        <div className="hamburger">
-          <MenuRoundedIcon onClick={() => setMenuOpen(!menuOpen)}/>
+        <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+          <span className={`menu-icon ${menuOpen ? 'hide': ''}`} >
+            <MenuRoundedIcon />
+          </span>
+
+          <span className={`close-icon ${menuOpen ? 'show': ''}`}>
+            <CloseRoundedIcon  />
+          </span>
         </div>
+
       </div>
 
-      <nav className='navbar-options'>
+      <nav className={`navbar-options ${menuOpen ? 'open' : ''}`}>
         <ul>
-          <li>HOME</li>
-          <li>FEATURES</li>
-          <li>HOW IT WORKS</li>
-          <li>CONTACT</li>
+          <li><Link to='/' className={location.pathname === '/' || location.pathname === '/home' ? 'active-option' : 'option-link'} style={{textDecoration: 'none'}}>HOME</Link></li>
+          <li><Link to='/' className={location.pathname === '/features' ? 'active-option' : 'option-link'} style={{textDecoration: 'none'}}>FEATURES</Link></li>
+          <li><Link to='/' className={location.pathname === '/howitworks' ? 'active-option' : 'option-link'} style={{textDecoration: 'none'}}>HOW IT WORKS</Link></li>
+          <li><Link to='/' className={location.pathname === '/contact' ? 'active-option' : 'option-link'} style={{textDecoration: 'none'}}>CONTACT</Link></li>
           <Link className='navbar-link' to='/login'>LOGIN</Link>
           <Link className='navbar-link' to='/register'>SIGN UP</Link>
         </ul>
-
-        
       </nav>
+      
+      </div>
 
     </div>
   )
